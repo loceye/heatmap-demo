a5d1b7a02cbbb5a1ba55000acce174241b144f1b
draptis <dimitrisraptis96@gmail.com>
2019-07-11 12:10:38 +0300

    setup svgr and generate icons

---
+++ src/components/Icons/Delete.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SvgDelete = props => (
+  <svg width={24} height={24} {...props}>
+    <path d="M19 6v12.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 5 18.5V6h-.5a.5.5 0 0 1 0-1H9v-.5A1.5 1.5 0 0 1 10.5 3h3A1.5 1.5 0 0 1 15 4.5V5h4.5a.5.5 0 1 1 0 1H19zM6 6v12.5A1.5 1.5 0 0 0 7.5 20h9a1.5 1.5 0 0 0 1.5-1.5V6H6zm8-1v-.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5V5h4zm0 4.5a.5.5 0 1 1 1 0v7a.5.5 0 1 1-1 0v-7zm-5 0a.5.5 0 0 1 1 0v7a.5.5 0 1 1-1 0v-7z" />
+  </svg>
+);
+
+export default SvgDelete;
---
+++ src/components/Icons/Refresh.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SvgRefresh = props => (
+  <svg width={24} height={24} {...props}>
+    <path
+      fill="currentColor"
+      d="M5 6.342A9 9 0 1 1 12 21c-4.765 0-8.696-3.713-8.983-8.447a.5.5 0 0 1 .998-.06A8 8 0 1 0 5.755 7H8.5a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 1 0v2.842z"
+    />
+  </svg>
+);
+
+export default SvgRefresh;
